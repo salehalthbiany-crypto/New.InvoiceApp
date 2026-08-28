c395af780fa24a41458df13cca524131ebbf870f
salehalthbiany-crypto <salehalthbiany@gmail.com>
2026-08-28 21:08:45 +0300

    Update AlEzzi Invoice App for app/src/main/java/com/alezzi/invoicetracker/InvoiceItem.kt

---
+++ app/src/main/java/com/alezzi/invoicetracker/InvoiceItem.kt
@@ -0,0 +1,8 @@
+package com.alezzi.invoicetracker
+
+data class InvoiceItem(
+    var id: Long = System.currentTimeMillis(),
+    var amount: Double = 0.0,
+    var details: String = "",
+    var balance: Double = 0.0
+)
